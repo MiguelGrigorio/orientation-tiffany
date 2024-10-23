7cdca45e3854f192f154b1d32ba178ffc36ff580
MiguelGrigorio <miguelgrigorio@hotmail.com>
2024-10-23 10:50:57 -0300

    Observações das funções adicionadas

--- src/main/main.py
+++ src/main/main.py
@@ -1,13 +1,11 @@
 from utils.code.classes.LoadCameraParameters import LoadCameraParameters
 from utils.code.functions.angle import angle
 from utils.code.getImages import getImages
-from utils.code.functions.undistort import undistort
-from utils.code.functions.convertCoords import world2pixel
+from utils.code.functions.convertCoords import world2pixel, undistort, pixel2world
 import numpy as np
 import cv2
 
-cameraList = [1, 2, 3, 4]
-selectCameras = cameraList
+selectCameras = [2]
 
 configPath = 'etc/calibrations/'
 tiffanyPath = 'assets/images/camera/'
@@ -19,9 +17,10 @@ cameraPath = 'assets/images/photos/'
 tiffany_2 = (620, 185, 55, 35)
 
 # region Exemplo de pontos
-#points = { 'bola': {}, 'frente': {} } # Para selecionar os pontos
-#points = {'bola': {1: (351.0, 172.0), 2: (650.0, 195.0), 4: (518.0, 308.0)}, 'frente': {1: (361.0, 181.0), 2: (639.0, 204.0), 4: (535.0, 294.0)}} # Pontos já selecionados
+
+points = {'bola': {1: (351.0, 172.0), 2: (650.0, 195.0), 4: (518.0, 308.0)}, 'frente': {1: (361.0, 181.0), 2: (639.0, 204.0), 4: (535.0, 294.0)}} # Pontos já selecionados
 points = { 'tiffany': {2: (645, 200)} } # Próximo do centro da tiffany
+
 # endregion
 
 for where in points.keys():
@@ -41,137 +40,107 @@ def LoadParameters(configPath: str) -> list:
 
 parameters = LoadParameters(configPath)
 
-pixels = np.empty((0, 2))
+# region Testes
 
+# Xw = np.array([[0.0], [0.0], [0.0], [1.0]])
+# world2pixel(parameters, tiffanyPath, selectCameras, Xw, True)
+
+
+# Basicamente o meu problema
+# locs = [None]
+# for id in [1, 2, 3, 4]:
+#     if id not in selectCameras:
+#         locs.append(None)
+#         continue
+#     p = np.array([[points['bola'][id][0]], [points['bola'][id][1]], [1]])
+#     locs.append(p)
+
+# Xws_bola = pixel2world(parameters, tiffanyPath, selectCameras, locs)
+
+# locs = [None]
+# for id in [1, 2, 3, 4]:
+#     if id not in selectCameras:
+#         locs.append(None)
+#         continue
+#     p = np.array([[points['frente'][id][0]], [points['frente'][id][1]], [1]])
+#     locs.append(p)
+
+# Xws_frente = pixel2world(parameters, tiffanyPath, selectCameras, locs)
+
+# vector = np.subtract(Xws_frente[:2], Xws_bola[:2])
+# vector = [float(vector[0]), float(vector[1])]
+# vector = [round(vector[0], 2), round(vector[1], 2)]
+# print('Vetor:', vector)
+# graus = angle(vector)
+# graus = round(graus, 2)
+# print('Ângulo:', graus)
+# endregion
+
+
+# Tenho que fazer o contrário aqui, dividir pela proporção do resize, e somar o x e y do roi e teoricamente, teria o ponto correto no mundo
+location = points['tiffany'][2]
+_, _, roi, _ = undistort(parameters, tiffanyPath, selectCameras)
+def tiffany(roi, image, location: list):
+    x, y, w, h = roi
+    newLoc = [location[0] - x, location[1] - y]
+    return image[y:y+h, x:x+w, :], newLoc
+
+Tiffany, newLoc = tiffany(tiffany_2, roi[2], location)
+shape = Tiffany.shape
+prop = 500 / shape[1]
+newLoc = [int(newLoc[0] * prop), int(newLoc[1] * prop)]
+Tiffany = cv2.resize(Tiffany, None, fx=prop, fy=prop)
+cv2.circle(Tiffany, newLoc, 3, (255, 0, 255), -1)
+cv2.imshow('Tiffany', Tiffany)
+cv2.waitKey(0)
+
+pointss = [None]
+for id in [1, 2, 3, 4]:
+    if id not in selectCameras:
+        pointss.append(None)
+        continue
+    pp = np.array([[points['tiffany'][id][0]], [points['tiffany'][id][1]], [1]])
+    pointss.append(pp)
+p = pixel2world(parameters, tiffanyPath, selectCameras, pointss)
+print(p)
+
+
+# region Provisório para seleção de pontos
+
+pixels = np.empty((0, 2))
 def getxy(event, x, y, flags, param):
     global pixels
     if event == cv2.EVENT_LBUTTONDOWN:
         pixels = np.vstack([pixels, np.hstack([x,y])])
+        cv2.destroyAllWindows()
 
-
-# Exemplo 1: Ponto no mundo para pixel
-def example1(parameters: list, show: bool = False) -> None:
-    # Transformação de cada câmeraa
-    for id in cameraList:
-        name = f'camera{id}'
-        params = parameters[id]
-        K = params.K
-        R = params.R
-        T = params.T
-        res = params.res
-        dis = params.dis
-
-        # Ponto no mundo
-        #Xw = np.array([[-2.02], [-3.07], [1.3], [1.0]]) 
-        Xw = np.array([[0.0], [0.0], [0.0], [1.0]]) 
-        Rtcw = np.concatenate([R, T], axis=1)
-
-        Xc = Rtcw @ Xw
-
-        image = cv2.imread(tiffanyPath+name+'.jpg')
-        img = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
-
-        w, h = res
-
-        # Corrige a matriz K para levar em consideração a nova imagem sem disorção
-        newK, roi = cv2.getOptimalNewCameraMatrix(K, dis,(w,h),1,(w,h))
-
-        dst = cv2.undistort(img, K, dis, None, newK)
-        
-        dst = cv2.cvtColor(dst, cv2.COLOR_RGB2BGR)
-
-        x,y,w,h = roi
-        newK[0, 2] = newK[0, 2] - x
-        newK[1, 2] = newK[1, 2] - y
-        u = newK @ Xc
-        z = u[2]
-        u = u / z
-
-        loc = (int(u[0][0]), int(u[1][0]))
-
-        dst = dst[y:y+h, x:x+w, :]
-        if show:
-            cv2.circle(dst, loc, 10, (0, 0, 255), -1)
-            cv2.imshow(name, dst)
-            cv2.waitKey(0)
-
-P, dst, roi, newK = undistort(parameters, tiffanyPath, selectCameras)
-Xw = np.array([[0.0], [0.0], [0.0], [1.0]])
-
-
-pixel = world2pixel(roi[2], newK, parameters[2].Rtcw, Xw, True)
-
-cv2.circle(dst[2], pixel, 10, (0, 0, 255), -1)
-cv2.imshow('tiffany', dst[2])
-cv2.waitKey(0)
-
-
-def tiffany(roi, image):
-    x, y, w, h = roi
-    return image[y:y+h, x:x+w, :]
-tiffany = tiffany(tiffany_2, roi[2])
-shape = tiffany.shape
-prop = 500 / shape[1]
-
-tiffany = cv2.resize(tiffany, None, fx=prop, fy=prop)
-cv2.imshow('tiffany', tiffany)
-cv2.waitKey(0)
-
-
-select = True if points['bola'] == {} else False
-if select:
-    for id in cameraList:
+def selectPoints(selectCameras: list):
+    points = { 'bola': {}, 'frente': {} }
+    _, _, roi, _ = undistort(parameters, tiffanyPath, selectCameras)
+    for id in [1, 2, 3, 4]:
         if id not in selectCameras:
             continue
-        name = f'camera{id}'
-        image = cv2.imread(imagePath+name+'.jpg')
+
+        name = f'Selecione o centro do ponto (Camera {id})'
         cv2.imshow(name, roi[id])
         cv2.setMouseCallback(name, getxy)
+        while pixels.shape[0] < 1:
+            cv2.waitKey(1)
+        points['bola'][id] = (int(pixels[0][0]), int(pixels[0][1]))
+        cv2.circle(roi[id], points['bola'][id], 3, (255, 0, 255), -1)
+
+        name = f'Selecione a frente da Tiffany (Camera {id})'
+        cv2.imshow(name, roi[id])
+        cv2.setMouseCallback(name, getxy)
+        while pixels.shape[0] < 2:
+            cv2.waitKey(1)
+        points['frente'][id] = (int(pixels[1][0]), int(pixels[1][1]))
+        cv2.circle(roi[id], points['frente'][id], 3, (255, 255, 0), -1)
+
+        name = f'Pontos selecionados (Camera {id})'
+        cv2.imshow(name, roi[id])
         cv2.waitKey(0)
-        points['bola'][id] = (pixels[0][0], pixels[0][1])
-        points['frente'][id] = (pixels[1][0], pixels[1][1])
         pixels = np.empty((0, 2))
+    return points
 
-A = { 'bola': {}, 'frente': {} }
-U = { 'bola': {}, 'frente': {} }
-
-for where in points.keys():
-    for id in points[where].keys():
-        coordToU = np.array([[points[where][id][0]], [points[where][id][1]], [1]])
-
-        try:
-            U[where][id].append(coordToU)
-        except:
-            U[where][id] = [coordToU]
-
-for where in U.keys():
-    for id in selectCameras:
-        zeros = np.zeros((3, 1))
-        mtx = np.array(P[id])
-        for i in U[where].keys():
-            if i == id:
-                mtx = np.hstack((mtx, -U[where][id][0]))
-            else:
-                mtx = np.hstack((mtx, zeros))
-        if len(A[where]) == 0:
-            A[where] = mtx
-        else:
-            A[where] = np.vstack((A[where], mtx))
-
-vn = { 'bola': [], 'frente': [] }
-
-for where in vn.keys():
-    _, _, V_transpose = np.linalg.svd(A[where])
-    vn[where] = V_transpose[-1]
-    vn[where] = vn[where] / vn[where][3]
-
-coord = { 'bola': [], 'frente': [] }
-
-for where in vn.keys():
-    for i in range(3):
-        coord[where].append(vn[where][i])
-vector = np.subtract(coord['frente'], coord['bola'])
-vector = [vector[0], vector[1]]
-print('Vetor:', vector)
-graus = angle(vector)
-print('Ângulo:', graus)
+# endregion
--- src/main/utils/code/functions/convertCoords.py
+++ src/main/utils/code/functions/convertCoords.py
@@ -1,15 +1,103 @@
 import cv2
 import numpy as np
 
-def world2pixel(dst: np.ndarray, newK: np.array, Rtcw: np.array, Xw: np.array, show: bool = False) -> None:
-    Xc = Rtcw @ Xw
-    u = newK @ Xc
-    z = u[2]
-    u = u / z
+def undistort(parameters: list, imagePath: str, selectCameras: list) -> list:
+    # P de cada câmera (newK @ Rtcw)
+    P = [None]
+    distort = [None]
+    interestArea = [None]
+    newKs = [None]
+    for id in [1, 2, 3 ,4]:             # Todas as câmeras
+        if id not in selectCameras:
+            P.append(None)
+            distort.append(None)
+            interestArea.append(None)
+            continue
+        name = f'camera{id}'
+        params = parameters[id]
+        K = params.K
+        Rtcw = params.Rtcw
+        res = params.res
+        dis = params.dis
 
-    loc = (int(u[0][0]), int(u[1][0]))
+        image = cv2.imread(imagePath + name + '.jpg')
 
-    if show:
-        cv2.circle(dst, loc, 10, (0, 0, 255), -1)
-        cv2.imshow("Ponto global", dst)
-        cv2.waitKey(0)
+        img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        w, h = res
+
+        # Corrige a matriz K para levar em consideração a nova imagem sem disorção
+        newK, roi = cv2.getOptimalNewCameraMatrix(K, dis, res, 1, res)
+
+        dst = cv2.undistort(img, K, dis, None, newK)
+        
+        dst = cv2.cvtColor(dst, cv2.COLOR_RGB2BGR)
+
+        x, y, w, h = roi
+        newK[0, 2] = newK[0, 2] - x
+        newK[1, 2] = newK[1, 2] - y
+
+        # Área de interesse
+        roi = dst[y:y+h, x:x+w, :]
+        P.append(newK @ Rtcw)
+        distort.append(dst)
+        interestArea.append(roi)
+        newKs.append(newK)
+
+    return P, distort, interestArea, newKs
+
+def world2pixel(parameters: list, imagePath: str, selectCameras: list, Xw: np.array, show: bool = False) -> list[tuple]:
+    _, _, image, newKs = undistort(parameters, imagePath, selectCameras)
+    locs = []
+    for id in [1, 2, 3, 4]:
+        if not id in selectCameras:
+            locs.append(None)
+            continue
+        Rtcw = parameters[id].Rtcw
+        Xc = Rtcw @ Xw
+        u = newKs[id] @ Xc
+        z = u[2]
+        u = u / z
+
+        loc = [int(u[0][0]), int(u[1][0])]
+        locs.append(loc)
+        if show:
+            cv2.circle(image[id], loc, 10, (0, 0, 255), -1)
+            cv2.imshow("Ponto global", image[id])
+            cv2.waitKey(0)
+    return locs
+
+def pixel2world(parameters: list, imagePath: str, selectCameras: list, locs: list) -> np.array:
+    P, _, _, _ = undistort(parameters, imagePath, selectCameras)
+    # mtxU é a localização do ponto em cada câmera em pixels
+    # mtxP é newK @ Rtcw
+    # mtxA é a matriz para resolver o sistema de equações com svd
+    # exemplo: mtxA = [P1 -u1 zeros; P2 zeros -u2]
+    # Xw é o ponto global
+
+    u = locs
+    A = []
+    for id in [1, 2, 3, 4]:
+        if not id in selectCameras:
+            continue
+        zeros = np.zeros((3, 1))
+        mtx = np.array(P[id])
+
+        # Cada linha
+
+        for i in selectCameras:
+            if i == id:
+                mtx = np.hstack((mtx, -u[id]))
+            else:
+                mtx = np.hstack((mtx, zeros))
+        # Formando a matriz A
+        if len(A) == 0:
+            A = mtx
+        else:
+            A = np.vstack((A, mtx))
+
+    _, _, V_transpose = np.linalg.svd(A)
+    V = V_transpose[-1]
+    V = V / V[3]
+    Xw = V[:3]
+    return Xw
